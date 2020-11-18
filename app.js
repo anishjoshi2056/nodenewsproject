ada68660c0fae6781e5025eaa0d3c9275ff5d1d0
Anish Joshi <anishjoshi2056@hotmail.com>
2020-11-18 11:58:37 +0545

    creating our app phase 2 completed

--- app.js
+++ app.js
@@ -4,10 +4,19 @@ const port = 5000;
 
 //Static Files
 app.use(express.static('public'));
-app.use('/css',express.static(__dirname + "public/css")) // to replicate press alt + shift + down
-app.use('/images',express.static(__dirname + "public/images"))
-app.use('/js',express.static(__dirname + "public/js"))
+app.use('/css',express.static(__dirname + "public/css")); // to replicate press alt + shift + down
+app.use('/images',express.static(__dirname + "public/images"));
+app.use('/js',express.static(__dirname + "public/js"));
+
+//Templating and view engine
+app.set('views','./src/views');
+app.set('view engine','ejs');
+
+// Routes
+const newsRouter = require('./src/routes/news')
+
+app.use('/', newsRouter)
 
 app.listen(port,()=> {
-    console.log(`Listening to the port ${port}`)
+    console.log(`Listening to the port ${port}`);
 })
